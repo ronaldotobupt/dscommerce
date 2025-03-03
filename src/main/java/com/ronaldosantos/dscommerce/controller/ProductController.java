e5d1259d6624b0b953d4ae2be027ef476ae2c52e
Ronaldo Santos <ronaldo.tboupt@gmail.com>
2025-03-03 09:30:27 -0300

    CRUD - Delete

--- src/main/java/com/ronaldosantos/dscommerce/controller/ProductController.java
+++ src/main/java/com/ronaldosantos/dscommerce/controller/ProductController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,5 +43,10 @@ public class ProductController {
 		dto = service.update(id, dto);
 		return ResponseEntity.ok(dto);
 	}
-
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
