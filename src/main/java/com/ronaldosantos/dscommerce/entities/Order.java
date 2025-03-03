943520ddf4930631e562ad191cc05bcf2394254c
Ronaldo Santos <ronaldo.tboupt@gmail.com>
2025-03-03 07:44:02 -0300

    Muitos-para-muitos com classe de associação PARTE 1

--- src/main/java/com/ronaldosantos/dscommerce/entities/Order.java
+++ src/main/java/com/ronaldosantos/dscommerce/entities/Order.java
@@ -1,7 +1,9 @@
 package com.ronaldosantos.dscommerce.entities;
 
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -11,6 +13,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
@@ -34,6 +37,8 @@ public class Order {
 	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
 	private Payment payment;
 	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Order() {
 		
