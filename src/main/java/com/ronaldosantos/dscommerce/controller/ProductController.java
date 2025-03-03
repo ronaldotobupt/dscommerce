d9652361b2838d15e720fc6548427e94cc3b8155
Ronaldo Santos <ronaldo.tboupt@gmail.com>
2025-03-03 09:25:58 -0300

    CRUD - Update

--- src/main/java/com/ronaldosantos/dscommerce/controller/ProductController.java
+++ src/main/java/com/ronaldosantos/dscommerce/controller/ProductController.java
@@ -3,9 +3,11 @@ package com.ronaldosantos.dscommerce.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +35,12 @@ public class ProductController {
 	@PostMapping
 	public ProductDTO insert(@RequestBody  ProductDTO dto) {
 		return service.insert(dto);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ProductDTO>update(@PathVariable Long id, @RequestBody  ProductDTO dto){
+		dto = service.update(id, dto);
+		return ResponseEntity.ok(dto);
 	}
 
 }
