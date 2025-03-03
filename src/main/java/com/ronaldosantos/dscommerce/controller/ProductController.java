8860e4cedcde0ca3288b608c081a1cf1ce6f8b8a
Ronaldo Santos <ronaldo.tboupt@gmail.com>
2025-03-03 10:32:01 -0300

    Customizando a resposta da validação

--- src/main/java/com/ronaldosantos/dscommerce/controller/ProductController.java
+++ src/main/java/com/ronaldosantos/dscommerce/controller/ProductController.java
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ronaldosantos.dscommerce.dto.ProductDTO;
 import com.ronaldosantos.dscommerce.services.ProductService;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
@@ -34,12 +36,12 @@ public class ProductController {
 	}
 	
 	@PostMapping
-	public ProductDTO insert(@RequestBody  ProductDTO dto) {
+	public ProductDTO insert(@Valid @RequestBody  ProductDTO dto) {
 		return service.insert(dto);
 	}
 	
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<ProductDTO>update(@PathVariable Long id, @RequestBody  ProductDTO dto){
+	public ResponseEntity<ProductDTO>update(@PathVariable Long id, @Valid @RequestBody  ProductDTO dto){
 		dto = service.update(id, dto);
 		return ResponseEntity.ok(dto);
 	}
